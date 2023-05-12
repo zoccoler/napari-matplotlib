00446f9e45c00e7bd37f963c9f0275bc200f2fc9
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-05-13 00:11:35 +0200

    WIP: new interactive lines

--- examples/test_extra_button_and_span_selector.py
+++ examples/test_extra_button_and_span_selector.py
@@ -9,18 +9,19 @@ import matplotlib.pyplot as plt
 # Read video
 
 url_data = r"C:\Users\mazo260d\Documents\GitHub\metroid\Data\Cell1\videos_AP\vid1.tif"
-url_data = r"Z:\Programming\metroid-master\Data\Cell1\videos_AP\vid1.tif"
+# url_data = r"Z:\Programming\metroid-master\Data\Cell1\videos_AP\vid1.tif"
 video_AP1 = imread(url_data)
 video_AP1 = video_AP1[:, np.newaxis]
 frame_rate_AP1 = 55.78
 
 url_data = r"C:\Users\mazo260d\Documents\GitHub\metroid\Data\Cell1\video_EP\cellmask.tif"
-url_data = r"Z:\Programming\metroid-master\Data\Cell1\video_EP\cellmask.tif"
+# url_data = r"Z:\Programming\metroid-master\Data\Cell1\video_EP\cellmask.tif"
 mask = imread(url_data).astype(bool)
 
-frame_rate = 60 # Hz or fps
+frame_rate = 60  # Hz or fps
 
 # Get signal and time array
+
 
 def get_masked_mean_over_time(timelapse_image, binary_mask):
     '''
@@ -29,13 +30,15 @@ def get_masked_mean_over_time(timelapse_image, binary_mask):
     '''
     # Remove extra Z dimension
     timelapse_image = timelapse_image.squeeze()
-    # If mask is 2D, broadcast it to same shape as timelapse_image 
+    # If mask is 2D, broadcast it to same shape as timelapse_image
     mask_broadcasted = np.broadcast_to(binary_mask, timelapse_image.shape)
     # Create masked array
-    timelapse_image_masked = np.ma.array(timelapse_image, mask = ~mask_broadcasted)
+    timelapse_image_masked = np.ma.array(
+        timelapse_image, mask=~mask_broadcasted)
     # Get mean over time of masked array
     signal = timelapse_image_masked.mean(axis=(-2, -1)).data
     return signal
+
 
 time_array_AP1 = np.arange(video_AP1.shape[0])/frame_rate_AP1
 AP_signal = get_masked_mean_over_time(video_AP1, mask)
@@ -65,8 +68,6 @@ metadata_AP1 = {}
 metadata_AP1['plugin1'] = {}
 metadata_AP1['plugin1']['signal'] = AP_signal
 metadata_AP1['plugin1']['time'] = time_array_AP1
-
-
 
 
 image_layer_AP.metadata = metadata_AP1
--- src/napari_matplotlib/line.py
+++ src/napari_matplotlib/line.py
@@ -19,6 +19,27 @@ from matplotlib.widgets import SpanSelector
 ICON_ROOT = Path(__file__).parent / "icons"
 __all__ = ["Line2DBaseWidget", "MetadataLine2DWidget"]
 
+# Define a custom subclass of Line2D with a custom property
+
+
+class InteractiveLine2D(Line2D):
+    def __init__(self, *args, selected=False, **kwargs, ):
+        super().__init__(*args, **kwargs)
+        self._selected = None
+
+    @property
+    def selected(self):
+        return self._selected
+
+    @selected.setter
+    def selected(self, value):
+        self._selected = value
+        if value == True:
+            self.set_linestyle('--')
+        elif value == False:
+            self.set_linestyle('-')
+        self.figure.canvas.draw_idle()
+
 
 class Line2DBaseWidget(NapariMPLWidget):
     # opacity value for the lines
@@ -35,7 +56,6 @@ class Line2DBaseWidget(NapariMPLWidget):
         self.span_selector = None
         self.legend_selection = False
         self.cid = None
-        
 
     def clear(self) -> None:
         """
@@ -63,14 +83,23 @@ class Line2DBaseWidget(NapariMPLWidget):
         for i, y in enumerate(y_data):
             label_name = y_axis_name
             if len(y_data) > 1:
-                label_name +=  '_' + str(i)
+                label_name += '_' + str(i)
             if len(x_data) == 1:
-                line = self.axes.plot(x_data[0], y, alpha=self._line_alpha, label=label_name)
+                line = InteractiveLine2D(
+                    x_data=x_data[0], ydata=y, alpha=self._line_alpha, label=label_name, linestyle='-', picker=True, pickradius=5)
+                self.axes.add_line(line)
+                # line = self.axes.plot(
+                #     x_data[0], y, alpha=self._line_alpha, label=label_name)
             else:
-                line = self.axes.plot(x_data[i], y, alpha=self._line_alpha, label=label_name)
-            self._lines += line
+                line2 = InteractiveLine2D(
+                    xdata=x_data[i], ydata=y, alpha=self._line_alpha, label=label_name, linestyle='-', picker=True, pickradius=5)
+                self.axes.add_line(line2)
+                # line = self.axes.plot(
+                #     x_data[i], y, alpha=self._line_alpha, label=label_name, color='red', linestyle='--')
+            # self._lines += [line]
         self.axes.set_xlabel(x_axis_name)
         self.axes.set_ylabel(y_axis_name)
+        self.axes.autoscale(enable=True, axis='both', tight=True)
         self.legend = self.axes.legend(fancybox=True, shadow=True)
 
         # If legend_selection is enabled, store relation between legend lines and lines
@@ -78,7 +107,8 @@ class Line2DBaseWidget(NapariMPLWidget):
         if self.legend_selection:
             self.legend2line = {}  # Will map legend lines to original lines.
             for legend_line, original_line in zip(self.legend.get_lines(), self._lines):
-                legend_line.set_picker(True)  # Enable picking on the legend line.
+                # Enable picking on the legend line.
+                legend_line.set_picker(True)
                 self.legend2line[legend_line] = original_line
 
     def _get_data(self) -> Tuple[List[np.ndarray], str, str]:
@@ -107,7 +137,7 @@ class Line2DBaseWidget(NapariMPLWidget):
         raise NotImplementedError
 
     def _create_span_selector(self, active=False,
-        *args, **kwargs):
+                              *args, **kwargs):
         """
         Create span selector.
 
@@ -119,7 +149,7 @@ class Line2DBaseWidget(NapariMPLWidget):
     def _enable_span_selector(self, active=False):
         """
         Enable or disable span selector.
-        
+
         If span selector was created, enable or disable it.
         """
         if self.span_selector is not None:
@@ -132,7 +162,7 @@ class Line2DBaseWidget(NapariMPLWidget):
         Get xmin and xmax of selection
         """
         raise NotImplementedError
-    
+
     def _enable_legend_selection(self, active=False):
         """
         Enable or disable making legend pickable.
@@ -152,7 +182,8 @@ class Line2DBaseWidget(NapariMPLWidget):
         Link mouse clicks to `onclick` callback function
         """
         if active:
-            self.cid = self.canvas.figure.canvas.mpl_connect('button_press_event', self._on_click)
+            self.cid = self.canvas.figure.canvas.mpl_connect(
+                'button_press_event', self._on_click)
         else:
             self.canvas.figure.canvas.mpl_disconnect(self.cid)
 
@@ -173,8 +204,8 @@ class Line2DBaseWidget(NapariMPLWidget):
         """
         if event.xdata is not None:
             print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
-          ('double' if event.dblclick else 'single', event.button,
-           event.x, event.y, event.xdata, event.ydata))
+                  ('double' if event.dblclick else 'single', event.button,
+                   event.x, event.y, event.xdata, event.ydata))
 
 
 class MetadataLine2DWidget(Line2DBaseWidget):
@@ -186,7 +217,7 @@ class MetadataLine2DWidget(Line2DBaseWidget):
         self._plugin_name_widget = magicgui(
             self._set_plugin_name,
             plugin_name={"choices": self._get_plugin_metadata_key},
-            auto_call = True,
+            auto_call=True,
         )
         self._key_selection_widget = magicgui(
             self._set_axis_keys,
@@ -194,26 +225,28 @@ class MetadataLine2DWidget(Line2DBaseWidget):
             y_axis_key={"choices": self._get_valid_axis_keys},
             call_button="plot",
         )
-        
+
         self.layout().insertWidget(0, self._plugin_name_widget.native)
         self.layout().addWidget(self._key_selection_widget.native)
 
         # Add span selection button to toolbar
         image_file_path = os.path.join(ICON_ROOT, "Select.png")
-        self.toolbar._add_new_button('Select', 'Span Selection', image_file_path, self.enable_span_selector, True)
+        self.toolbar._add_new_button(
+            'Select', 'Span Selection', image_file_path, self.enable_span_selector, True)
         # Cache lists
         self._selected_span_intervals = []
         self._selected_lines = []
 
         # Create horizontal Span Selector
         self._create_span_selector(ax=self.axes,
-            onselect=self._on_span_select,
-            direction="horizontal",
-            useblit=True,
-            props=dict(alpha=0.5, facecolor="tab:orange"),
-            interactive=False,
-            button=1,
-            drag_from_anywhere=True)
+                                   onselect=self._on_span_select,
+                                   direction="horizontal",
+                                   useblit=True,
+                                   props=dict(
+                                       alpha=0.5, facecolor="tab:orange"),
+                                   interactive=False,
+                                   button=1,
+                                   drag_from_anywhere=True)
 
         # Enable legend selection
         self._enable_legend_selection(True)
@@ -222,7 +255,7 @@ class MetadataLine2DWidget(Line2DBaseWidget):
 
     def _on_click(self, event):
         # Right click clears selections
-        if event.button==3: 
+        if event.button == 3:
             self._clear_selections()
 
     def _clear_selections(self):
@@ -236,12 +269,12 @@ class MetadataLine2DWidget(Line2DBaseWidget):
                 line.set_visible(True)
         # Clear span selections if span selector exists
         if self.span_selector is not None:
-            self._on_span_select(0,0)
+            self._on_span_select(0, 0)
         self.canvas.figure.canvas.draw()
 
     # Callback function from toolbar span selection toggle button
     def enable_span_selector(self):
-        self.toolbar._update_buttons_checked(button_name = 'Select')
+        self.toolbar._update_buttons_checked(button_name='Select')
         self._enable_span_selector(active=self.toolbar.button_state)
 
     def _on_legend_selection(self, selected_artist):
@@ -308,17 +341,25 @@ class MetadataLine2DWidget(Line2DBaseWidget):
                     # TO DO: test if data is not a numpy array
                     if (modifiers == Qt.ShiftModifier) and (len(self._selected_span_intervals) == len(self._lines)):
                         # Plot and store selected points/line
+                        # interval = InteractiveLineInterval2D(xdata=self.axes.plot(np.concatenate((self._selected_span_intervals[i].get_xdata(), region_x)),
+                        #                         ydata=np.concatenate((self._selected_span_intervals[i].get_ydata(), region_y)),
+                        #                         alpha=self._line_alpha, markerstyle='o', picker=True, pickradius=5)
+                        # self.axes.add_line(line)
                         selected_interval = self.axes.plot(np.concatenate((self._selected_span_intervals[i].get_xdata(), region_x)),
-                        np.concatenate((self._selected_span_intervals[i].get_ydata(), region_y)), 'o')
-                        self._selected_span_intervals[i] = selected_interval[0] # it needs index 0, otherwise inserts unitary list
+                                                           np.concatenate((self._selected_span_intervals[i].get_ydata(), region_y)), 'o')
+                        # it needs index 0, otherwise inserts unitary list
+                        self._selected_span_intervals[i] = selected_interval[0]
                     else:
-                        selected_interval = self.axes.plot(region_x, region_y, 'o')
+                        selected_interval = self.axes.plot(
+                            region_x, region_y, 'o')
                         self._selected_span_intervals += selected_interval
             self.canvas.draw_idle()
-                    
+
             # Store selected regions in new metadata key
-            self.layers[0].metadata[self.plugin_name_key]['selected_interval_' + self.x_axis_key] = [line.get_xdata() for line in self._selected_span_intervals]
-            self.layers[0].metadata[self.plugin_name_key]['selected_interval_' + self.y_axis_key] = [line.get_ydata() for line in self._selected_span_intervals]
+            self.layers[0].metadata[self.plugin_name_key]['selected_interval_' +
+                                                          self.x_axis_key] = [line.get_xdata() for line in self._selected_span_intervals]
+            self.layers[0].metadata[self.plugin_name_key]['selected_interval_' +
+                                                          self.y_axis_key] = [line.get_ydata() for line in self._selected_span_intervals]
 
     @property
     def x_axis_key(self) -> Optional[str]:
@@ -345,7 +386,7 @@ class MetadataLine2DWidget(Line2DBaseWidget):
         self._x_axis_key = x_axis_key
         self._y_axis_key = y_axis_key
         self._draw()
-        
+
     @property
     def plugin_name_key(self) -> Optional[str]:
         """Key to plugin dictionary in the Metadata"""
@@ -354,20 +395,20 @@ class MetadataLine2DWidget(Line2DBaseWidget):
     @plugin_name_key.setter
     def plugin_name_key(self, key: Optional[str]) -> None:
         self._plugin_name_key = key
-        
+
     def _set_plugin_name(self, plugin_name: str) -> None:
         """Set plugin name from layer metadata"""
         self._plugin_name_key = plugin_name
         self._key_selection_widget.reset_choices()
-    
+
     def _get_plugin_metadata_key(
-            self, combo_widget: Optional[ComboBox] = None
-        ) -> List[str]:
+        self, combo_widget: Optional[ComboBox] = None
+    ) -> List[str]:
         """Get plugin key from layer metadata"""
         if len(self.layers) == 0:
             return []
         else:
-            return self._get_valid_metadata_keys() 
+            return self._get_valid_metadata_keys()
 
     def _get_valid_metadata_keys(
             self) -> List[str]:
@@ -393,7 +434,7 @@ class MetadataLine2DWidget(Line2DBaseWidget):
             The valid axis keys in the Metadata. If the table is empty
             or there isn't a table, returns an empty list.
         """
-        
+
         if len(self.layers) == 0:
             return []
         else:
@@ -420,14 +461,14 @@ class MetadataLine2DWidget(Line2DBaseWidget):
             an empty string if nothing to plot.
         """
         valid_metadata = self._get_valid_metadata_keys()
-        
+
         if (
             (not valid_metadata)
             or (self.x_axis_key is None)
             or (self.y_axis_key is None)
         ):
             return [], "", ""
-        
+
         if not hasattr(self, "plugin_name_key"):
             self.plugin_name_key = valid_metadata[0]
 
@@ -450,18 +491,19 @@ class MetadataLine2DWidget(Line2DBaseWidget):
         if hasattr(self, "_key_selection_widget"):
             self._plugin_name_widget.reset_choices()
             self._key_selection_widget.reset_choices()
-        
+
         # reset the axis keys
         self._x_axis_key = None
         self._y_axis_key = None
-        
+
+
 def warp_to_list(data):
     if isinstance(data, list):
         return data
     # If numpy array, make a list from axis=0
     if isinstance(data, np.ndarray):
         if len(data.shape) == 1:
-            data = data[np.newaxis,:]
+            data = data[np.newaxis, :]
         data = data.tolist()
     # If pandas dataframe, make a list from columns
     if isinstance(data, pd.DataFrame):
